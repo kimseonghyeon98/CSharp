b155c0467f3d73321ed3308aec23d77380a66904
kimseonghyeon98 <80227943+kimseonghyeon98@users.noreply.github.com>
2021-04-26 18:49:48 +0900

    Add files via upload

---
+++ 0426/DerivedConstructorApp.cs
@@ -0,0 +1,25 @@
+using System;
+namespace DerivedConstructorApp
+{
+    class BaseClass
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("BaseClass Constructor ...");
+        }
+    }
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass()
+        {
+            Console.WriteLine("DerivedClass Constructor ...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass obj = new DerivedClass(); Console.WriteLine("In the main ...");
+        }
+    }
+}
